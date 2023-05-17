b69c8a388521208c89de20da301765c0231b2201
I am breachers <89657427+breachers-op@users.noreply.github.com>
2023-05-17 15:01:56 +0530

    Add files via upload

---
+++ RIDER/__init__.py
@@ -0,0 +1,37 @@
+from RIDER.core.bot import RIDERBot
+from RIDER.core.dir import dirr
+from RIDER.core.git import git
+from RIDER.core.userbot import Userbot
+from RIDER.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+
+dirr()
+
+git()
+
+dbb()
+
+heroku()
+
+sudo()
+
+# Clients
+app = RIDERBot()
+
+userbot = Userbot()
+
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
