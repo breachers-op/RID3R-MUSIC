f9b921fa6cac2a20326932fd66fccc88a0370dc2
I am breachers <89657427+breachers-op@users.noreply.github.com>
2023-05-17 18:58:33 +0530

    Update __main__.py

--- RIDER/__main__.py
+++ RIDER/__main__.py
@@ -8,7 +8,7 @@ from pytgcalls.exceptions import NoActiveGroupCall
 import config
 from config import BANNED_USERS
 from RIDER import LOGGER, app, userbot
-from RIDER.core.call import Vip
+from RIDER.core.call import RIDER
 from RIDER.plugins import ALL_MODULES
 from RIDER.utils.database import get_banned_users, get_gbanned
 
